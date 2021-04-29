2e636c15f6fd9e28103464764da42a5653ea2d6f
bhoomika m <65276833+pinkfloyd369@users.noreply.github.com>
2021-04-29 12:04:26 +0530

    Update appdna.py

--- appdna.py
+++ appdna.py
@@ -1,15 +1,12 @@
 import pandas as pd
 import streamlit as st
 import altair as alt
-from PIL import Image
+
 
 ######################
 # Page Title
 ######################
 
-image = Image.open('dna-logo.jpg')
-
-st.image(image, use_column_width=True)
 
 st.write("""
 # DNA Nucleotide Count Web App
